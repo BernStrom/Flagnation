2b6a4eb2f5e51e00a3ea5f6e400ae0e93bb1b09e
Bern <bernnystrom@gmail.com>
2022-02-21 15:08:37 +0800

    Initial Commit

---
+++ Flagnation/FlagnationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagnationApp.swift
+//  Flagnation
+//
+//  Created by Bern N on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagnationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
