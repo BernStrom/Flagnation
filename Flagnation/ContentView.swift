200c019a9fce7cce8138dca93236f7b6af80995e
Bern <bernnystrom@gmail.com>
2022-02-25 15:00:27 +0800

    💄 (new): Change background color and fonts weight
    Changed the background color to use a custom LinearGradient and modified the fonts weight of the question with .font() modifiers.

--- Flagnation/ContentView.swift
+++ Flagnation/ContentView.swift
@@ -15,15 +15,17 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
+                        .font(.subheadline.weight(.heavy))
                         .foregroundColor(.white)
                     
                     Text(countries[correctAnswer])
+                        .font(.largeTitle.weight(.semibold))
                         .foregroundColor(.white)
                 }
                 
