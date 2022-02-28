be5cf2579150456b704176f5165152ff56c706de
Bern <bernnystrom@gmail.com>
2022-02-28 15:06:34 +0800

    💄 (new): Change background style and color
    Changed the app background style and color to use a custom RadialGradient.

--- Flagnation/ContentView.swift
+++ Flagnation/ContentView.swift
@@ -15,7 +15,10 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                Gradient.Stop(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                Gradient.Stop(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
